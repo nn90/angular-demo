036f1d6d523241e754f2397256ad2f481dbdadbf
nn90 <n9560027739@yandex.com>
2018-07-18 00:27:02 +0530

    Service ad DI

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { IProduct } from './products';
+import { ProductService } from '../product.service';
 
 @Component({
   selector: 'pm-product-list',
@@ -8,15 +9,17 @@ import { IProduct } from './products';
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit {
-
-  constructor() {
-    this.filteredProducts = this.products;
-    this.listFilter = 'Cart';
+  //Importing the service and the data
+  constructor(private productService: ProductService) {
+   
+    this.listFilter = '';
   }
 
   ngOnInit() {
     console.log('IN INIT')
     console.log(this)
+    this.products = this.productService.getProducts()
+    this.filteredProducts = this.products;
   }
   pageTitle: string = 'Product List!';
   imageWidth = 50;
@@ -35,28 +38,7 @@ export class ProductListComponent implements OnInit {
 
   filteredProducts: IProduct[] = [];
 
-  products: IProduct[] = [
-    {
-      "productId": 1,
-      "productName": "Leaf Rake",
-      "productCode": "GDN-0011",
-      "releaseDate": "March 19, 2016",
-      "description": "Leaf rake with 48-inch wooden handle.",
-      "price": 19.95,
-      "starRating": 3.2,
-      "imageUrl": "https://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png"
-    },
-    {
-      "productId": 2,
-      "productName": "Garden Cart",
-      "productCode": "GDN-0023",
-      "releaseDate": "March 18, 2016",
-      "description": "15 gallon capacity rolling garden cart",
-      "price": 32.99,
-      "starRating": 4.2,
-      "imageUrl": "https://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png"
-    }
-  ];
+  products: IProduct[] = [];
 
 
   onRatingClicked(message: string):void{
